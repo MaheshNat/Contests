b12211dc9c6fade33aeb244f6b7ce1ae849de496
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2019-12-01 17:20:11 -0600

    refactor to VSCode

---
+++ advent_of_code_2019/Constants.java
@@ -0,0 +1,3 @@
+public class Constants {
+    public static final String FILE_PATH = "/Users/mahesh/Documents/Contests/advent_of_code_2019/input/";
+}
---
+++ advent_of_code_2019/Day1.java
@@ -0,0 +1,26 @@
+import Constants;
+
+import java.io.*;
+import java.util.*;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new BufferedReader(new FileReader(Constants.FILE_PATH + "day1")));
+        int part1Sum = 0;
+        int part2Sum = 0;
+        while (in.hasNextInt()) {
+            int nextInt = in.nextInt();
+            part1Sum += nextInt / 3 - 2;
+            part2Sum += fuel(nextInt);
+        }
+        System.out.println("part1: " + part1Sum);
+        System.out.println("part2: " + part2Sum);
+    }
+
+    public static int fuel(int num) {
+        int fuel = num / 3 - 2;
+        if (fuel <= 0)
+            return 0;
+        return fuel + fuel(fuel);
+    }
+}
